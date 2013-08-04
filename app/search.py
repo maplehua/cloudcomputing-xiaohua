9a69a0996111fdb38a5eb06dbf504a139dce089c
Chengyin Xia <xia9139@163.com>
2013-08-04 17:27:10 +0800

    Duplication of Name Handled, using a list to choose, Search Bar should be improved.

--- app/search.py
+++ app/search.py
@@ -74,7 +74,7 @@ class AcademiSearch():
 
     def _scholar_search(self):
         keyword=self.keyword
-        scholar=get_scholar(keyword)
+        scholars=get_scholars(keyword)
         papers=get_papers(keyword)
         #stat = paper_en_es_stat(keywords)
         #result_list = paper_en_es_search(keywords, self.offset, RESULT_SIZE)
@@ -83,7 +83,7 @@ class AcademiSearch():
         #for result in result_list:
         #    meta_doc = paper_en_fetch_meta(result.uuid)
         #    papers.append(paper_en_rebuild(result, meta_doc))
-        return dict(scholar=scholar,
+        return dict(scholars = scholars,
                 papers = papers,
                 keyword= keyword)
 
@@ -272,15 +272,13 @@ def paper_en_rebuild(es_result, mongo_doc):
         new_result['explain'] = es_result._meta.explanation
     return new_result
 
-def get_scholar(keyword):
-    scholar=mongo_conn.Microsoft_AS.AuthorInfo.find_one({u"Name":keyword})
-    if scholar:
-        #if scholar[u"Photo"]==u"AAA":
-        #    scholar[u"Photo"]=default_ph
-        #scholar[u"Photo"]="data:image/gif;base64,"+scholar[u"Photo"]
-        return scholar
-    else:
-        return None
+def get_scholars(keyword):
+    sch_cur=mongo_conn.Microsoft_AS.AuthorInfo.find({u"Name":keyword})
+    scholars=[]
+    if sch_cur.count()>0:
+        for sch in sch_cur:
+            scholars.append(sch)
+    return scholars
 
 def get_papers(keyword):
     mycursor=mongo_conn.dblp.dblp_papers_all.find({"authors":keyword})
