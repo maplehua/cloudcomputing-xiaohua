2a7d0ebc2778a0221ee87c9b6489817a43d5b11a
caixiaohua <caixiaohua110@163.com>
2013-10-20 10:52:46 +0800

    wanna add count_paper_by_year  change 3files:models.py search.py result.html

--- app/models.py
+++ app/models.py
@@ -104,6 +104,7 @@ class Affiliation(db.Document):
         #page = int(page) if page else 1
         return papers.paginate(page = int(page), per_page = 10) if affi else None
 
+
     @classmethod
     def stat_papers(self, aff_name):
         affi = Affiliation.objects(name = aff_name).first()
@@ -125,3 +126,45 @@ class Affiliation(db.Document):
             prop_rank_b = prop_rank_b,
             prop_rank_c = prop_rank_c,
             prop_rank_unknow = prop_rank_unknow)
+
+
+     @classmethod
+     def get_year_papers(self,aff_name)
+         affi = Affiliation.objects(name = aff_name).first()
+         count_all = PaperMeta.objects(authors__in = affi.scholars).count() if affi else 0
+         count_rank_a_2013 = PaperMeta.objects(authors__in = affi.scholars, ccf_rank = 'A', year = "2013").count() if affi else 0
+         count_rank_b_2013 = PaperMeta.objects(authors__in = affi.scholars, ccf_rank = 'B', year = "2013").count() if affi else 0
+         count_rank_c_2013 = PaperMeta.objects(authors__in = affi.scholars, ccf_rank = 'C', year = "2013").count() if affi else 0
+         count_rank_a_2012 = PaperMeta.objects(authors__in = affi.scholars, ccf_rank = 'A', year = "2012").count() if affi else 0
+         count_rank_b_2012 = PaperMeta.objects(authors__in = affi.scholars, ccf_rank = 'B', year = "2012").count() if affi else 0
+         count_rank_c_2012 = PaperMeta.objects(authors__in = affi.scholars, ccf_rank = 'C', year = "2012").count() if affi else 0
+         count_rank_a_2011 = PaperMeta.objects(authors__in = affi.scholars, ccf_rank = 'A', year = "2011").count() if affi else 0
+         count_rank_b_2011 = PaperMeta.objects(authors__in = affi.scholars, ccf_rank = 'B', year = "2011").count() if affi else 0
+         count_rank_c_2011 = PaperMeta.objects(authors__in = affi.scholars, ccf_rank = 'C', year = "2011").count() if affi else 0
+         count_rank_a_2010 = PaperMeta.objects(authors__in = affi.scholars, ccf_rank = 'A', year = "2010").count() if affi else 0
+         count_rank_b_2010 = PaperMeta.objects(authors__in = affi.scholars, ccf_rank = 'B', year = "2010").count() if affi else 0
+         count_rank_c_2010 = PaperMeta.objects(authors__in = affi.scholars, ccf_rank = 'C', year = "2010").count() if affi else 0
+         count_rank_a_2009 = PaperMeta.objects(authors__in = affi.scholars, ccf_rank = 'A', year = "2009").count() if affi else 0
+         count_rank_b_2009 = PaperMeta.objects(authors__in = affi.scholars, ccf_rank = 'B', year = "2009").count() if affi else 0
+         count_rank_c_2009 = PaperMeta.objects(authors__in = affi.scholars, ccf_rank = 'C', year = "2009").count() if affi else 0
+         return dict(count_rank_a_2013=count_rank_a_2013,
+                    count_rank_a_2013=count_rank_a_2013,
+                    count_rank_a_2013=count_rank_a_2013,
+                    count_rank_a_2013=count_rank_a_2012,
+                    count_rank_a_2013=count_rank_a_2012,
+                    count_rank_a_2013=count_rank_a_2012,
+                    count_rank_a_2013=count_rank_a_2011,
+                    count_rank_a_2013=count_rank_a_2011,
+                    count_rank_a_2013=count_rank_a_2011,
+                    count_rank_a_2013=count_rank_a_2010,
+                    count_rank_a_2013=count_rank_a_2010,
+                    count_rank_a_2013=count_rank_a_2010,
+                    count_rank_a_2013=count_rank_a_2009,
+                    count_rank_a_2013=count_rank_a_2009,
+                    count_rank_a_2013=count_rank_a_2009)
+         
+         
+         
+         
+         
+
